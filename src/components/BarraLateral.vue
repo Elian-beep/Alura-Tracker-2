7140001b095499e1430840bd1080d086ad439625
Elian-beep <elianoliver101@gmail.com>
2022-10-28 12:07:16 -0300

    links routes

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,61 +1,94 @@
 <template>
-    <header>
-        <h1>
-            <img src="@/assets/logo.png" alt="logo">
-        </h1>
-        <button class="button" @click="alterarTema">
-            {{textoBotao}}
-        </button>
-    </header>
+  <header>
+    <h1>
+      <img src="@/assets/logo.png" alt="logo" />
+    </h1>
+    <div class="has-text-contered">
+      <button class="button" @click="alterarTema">
+        {{ textoBotao }}
+      </button>
+    </div>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link class="link" to="/">
+            <i class="fas fa-tasks"></i>
+            tarefas
+          </router-link>
+        </li>
+        <li>
+          <router-link class="link" to="/projetos">
+            <i class="fas fa-tasks"></i>
+            projetos
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'BarraLateral',
-    emits: ['aoTemaAlterado'],
-    data () {
-        return {
-            modoEscuroAtivo: false
-        }
+  name: "BarraLateral",
+  emits: ["aoTemaAlterado"],
+  data() {
+    return {
+      modoEscuroAtivo: false,
+    };
+  },
+  computed: {
+    textoBotao() {
+      if (this.modoEscuroAtivo) {
+        return "Desativar modo escuro";
+      } else {
+        return "Ativar modo escuro";
+      }
     },
-    computed: {
-        textoBotao(){
-            if(this.modoEscuroAtivo){
-                return 'Desativar modo escuro';
-            }else{
-                return 'Ativar modo escuro'
-            }
-        }
+  },
+  methods: {
+    alterarTema() {
+      this.modoEscuroAtivo = !this.modoEscuroAtivo;
+      this.$emit("aoTemaAlterado", this.modoEscuroAtivo);
     },
-    methods: {
-        alterarTema(){
-            this.modoEscuroAtivo = !this.modoEscuroAtivo;
-            this.$emit('aoTemaAlterado', this.modoEscuroAtivo);
-        }
-    }
+  },
 });
-
 </script>
 
 <style scoped>
-header{
-    background: #0d3b66;
-    width: 100%;
-    height: 100vh;
-    padding: 1rem;
-    text-align: center;
+header {
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+  padding: 1rem;
+  text-align: center;
 }
 
-img{
-    width: 100%;
+img {
+  width: 100%;
 }
 
 @media screen and (max-width: 768px) {
-    header{
-        padding: 2.5rem;
-        height: auto;
-    }
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+
+.panel li {
+  margin: 8px 0;
+}
+
+.link {
+  color: #fff;
+}
+
+.link:hover {
+  color: #faf0ca;
+}
+
+.link .router-link-active {
+  color: #faf0ca;
 }
 </style>
